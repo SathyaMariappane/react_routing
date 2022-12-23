3b810e3ba6397adffd588a711dfa85a077e5a37d
SathyaMariappane <119936708+SathyaMariappane@users.noreply.github.com>
2022-12-23 20:29:14 +0530

    initial commit

---
+++ src/Components/component1.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Component1=()=>{
+    return(
+        <div className="Sample2">
+            <p>Component 1</p>
+        </div>
+    )
+}
+export default Component1
---
+++ src/Components/component2.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Component2=()=>{
+    return(
+        <div className="Sample3">
+            <p>Component 2</p>
+        </div>
+    )
+}
+export default Component2
